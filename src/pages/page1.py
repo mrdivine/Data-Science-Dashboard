38811b8ff107740ed0f53220efbac8b29e109b30
dude1 <mathew.divine@gmail.com>
2024-10-24 13:55:26 +0200

    added side-panel

---
+++ src/pages/page1.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title("Page 1")
+    st.write("This is Page 1 of the dashboard.")
---
+++ src/pages/page2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title("Page 2")
+    st.write("This is Page 2 of the dashboard.")
